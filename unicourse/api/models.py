e4bf97f70d607c52eb82f29bfcc565ebc636de08
ladikusz <ladikszabolcs@draken.hu>
2024-11-22 14:59:43 +0100

    user model conflict fix

--- unicourse/api/models.py
+++ unicourse/api/models.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
 class User(AbstractUser):
@@ -8,6 +8,22 @@ class User(AbstractUser):
         ('student', 'Student'),
     )
     role = models.CharField(max_length=10, choices=ROLES)
+
+    # Avoid reverse accessor clashes by specifying related_name
+    groups = models.ManyToManyField(
+        Group,
+        related_name='custom_user_groups',
+        blank=True,
+        help_text='The groups this user belongs to.',
+        verbose_name='groups',
+    )
+    user_permissions = models.ManyToManyField(
+        Permission,
+        related_name='custom_user_permissions',
+        blank=True,
+        help_text='Specific permissions for this user.',
+        verbose_name='user permissions',
+    )
 
 class Lecture(models.Model):
     title = models.CharField(max_length=100)
