63d1f49e14750e2f206b827de4fa9a4c37bc995b
ladikusz <ladikszabolcs@draken.hu>
2024-12-05 14:37:29 +0100

    adding token renewal upon logout

--- unicourse/api/auth_urls.py
+++ unicourse/api/auth_urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from .views import CurrentUserView
-from rest_framework.authtoken.views import obtain_auth_token
+from .views import CustomAuthToken
 
 urlpatterns = [
-    path('token/', obtain_auth_token, name='api_token_auth'),
+    path('token/', CustomAuthToken.as_view(), name='api_token_auth'),
     path('me/', CurrentUserView.as_view(), name='auth_me'),
 ]
